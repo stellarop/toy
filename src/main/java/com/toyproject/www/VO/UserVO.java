93ee0c6e50ef212e116f0023b81c9a83f2c53aac
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-04-06 22:35:01 +0900

    userVO 생성

---
+++ src/main/java/com/toyproject/www/VO/UserVO.java
@@ -0,0 +1,61 @@
+package com.toyproject.www.VO;
+
+public class UserVO {
+	private String id;
+	private String password;
+	private String username;
+	private String email;
+	private String sex;
+	private int phonenumber;
+	private String address;
+	private String address2;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+}
