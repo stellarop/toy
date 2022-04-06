ebedc876871f09951aaa47db9349104752c6e846
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-04-06 22:36:05 +0900

    SimpleBoardVO 생성

---
+++ src/main/java/com/toyproject/www/VO/SimpleBoardVO.java
@@ -0,0 +1,40 @@
+package com.toyproject.www.VO;
+
+public class SimpleBoardVO {
+	private int board_idx;
+	private String type;
+	private String title;
+	private String writer;
+	private String content;
+	
+	public int getBoard_idx() {
+		return board_idx;
+	}
+	public void setBoard_idx(int board_idx) {
+		this.board_idx = board_idx;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
