a5f303b878f28c10eec98d76f1680dce66148554
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-04-10 00:09:22 +0900

    소문자로 수정

--- src/main/java/com/toyproject/www/DAO/UserDAO.java
+++ src/main/java/com/toyproject/www/DAO/UserDAO.java
@@ -1,5 +1,18 @@
 package com.toyproject.www.DAO;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.toyproject.www.VO.UserVO;
+
+@Repository
 public class UserDAO {
 
+	@Autowired
+	SqlSessionTemplate mybatis;
+	
+	public void insertUser(UserVO uservo) {
+		mybatis.insert("userDAO.insertUser",uservo);
+	}
 }
