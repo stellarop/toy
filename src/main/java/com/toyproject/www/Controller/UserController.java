cadf3ab206306c0c664bada344b647ef2f60e64d
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-04-10 00:36:39 +0900

    usercontroller 생성 , 회원가입 메서드, 로그인 메서드

--- src/main/java/com/toyproject/www/Controller/UserController.java
+++ src/main/java/com/toyproject/www/Controller/UserController.java
@@ -1,5 +1,25 @@
 package com.toyproject.www.Controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.toyproject.www.Service.UserService;
+import com.toyproject.www.VO.UserVO;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	UserService userservice;
+	
+	public void insertUser(UserVO uservo) {
+		userservice.insertUser(uservo);
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		System.out.println("로그인 메서드 통과");
+		return "login";
+	}
 }
