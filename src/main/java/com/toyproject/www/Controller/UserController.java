a03ce5f7c31f7ce512d1da6dde79e27f42e666e5
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-04-11 00:19:19 +0900

    로그인, 회원가입 경로 메서드

--- src/main/java/com/toyproject/www/Controller/UserController.java
+++ src/main/java/com/toyproject/www/Controller/UserController.java
@@ -1,8 +1,13 @@
 package com.toyproject.www.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.toyproject.www.Service.UserService;
 import com.toyproject.www.VO.UserVO;
@@ -13,14 +18,24 @@ public class UserController {
 	@Autowired
 	UserService userservice;
 	
-	public void SignUp(UserVO uservo) {
-		userservice.SignUp(uservo);
+	
+	
+	@RequestMapping(value = "/SignUp.do", method = RequestMethod.GET)
+	public String SignUp() {
+		return "SignUp";
 	}
 	
-	@RequestMapping("/login")
+	
+	@RequestMapping(value = "/SignUp.do", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> SignUp(UserVO uservo) {
+		HashMap<String, Object> SignUpMap = new HashMap<String, Object>();
+		//SignUpMap.put("SignUp", userservice.SignUp(uservo));
+		return SignUpMap;
+	}
+	
+	@RequestMapping("/login.do")
 	public String login() {
-		System.out.println("로그인 메서드 통과");
-		System.out.println("한글깨짐test");
 		return "login";
 	}
 }
