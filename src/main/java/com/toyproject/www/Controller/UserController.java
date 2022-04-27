e6b758c4e14775ee1d7b9878ae0541525219613e
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-04-27 12:57:58 +0900

    로그인 컨트롤러 추가

--- src/main/java/com/toyproject/www/Controller/UserController.java
+++ src/main/java/com/toyproject/www/Controller/UserController.java
@@ -1,9 +1,9 @@
 package com.toyproject.www.Controller;
 
-
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,37 +14,55 @@ import com.toyproject.www.VO.UserVO;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	UserService userservice;
-	
+
 	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
 	public String login() {
 		return "login.jsp";
 	}
-	
-	
+
+	@ResponseBody
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
+	public ResponseEntity<Integer> selectUser(UserVO uservo) {
+
+		try {
+			UserVO user = userservice.selectUser(uservo);
+
+			if (user != null || StringUtils.isEmpty(user.getId())) {
+				return new ResponseEntity<Integer>(1, HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			String error = e.getMessage();
+			int errorMessage = Integer.parseInt(error);
+			
+			return new ResponseEntity<Integer>(errorMessage, HttpStatus.BAD_REQUEST);
+		}
+		return null;
+
+	}
+
 	@RequestMapping(value = "/SignUp.do", method = RequestMethod.POST)
 	@ResponseBody
-	public boolean SignUp(UserVO uservo) {
-		
+	public boolean signUp(UserVO uservo) {
+
 		int idCheck = userservice.idCheck(uservo);
-		
-		if(idCheck == 1) {
+
+		if (idCheck == 1) {
 			return false;
-		}else {
+		} else {
 			userservice.SignUp(uservo);
 		}
 		return true;
 	}
-	
-	
+
 	@RequestMapping(value = "/idCheck.do")
 	@ResponseBody
 	public int idCheck(UserVO uservo) {
 		int idCheck = userservice.idCheck(uservo);
 		return idCheck;
 	}
-	
-	
+
 }
