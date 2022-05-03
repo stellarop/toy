3a12eb1af6c6b71c00e805f3473912b4d6da81d9
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-05-03 19:50:50 +0900

    vendorVO, productVO 생성

---
+++ src/main/java/com/toyproject/www/VO/ProductVO.java
@@ -0,0 +1,70 @@
+package com.toyproject.www.VO;
+
+public class ProductVO {
+	private int product_seq;
+	private String product_code;
+	private String vendor_name;
+	private String product_type;
+	private int product_price;
+	private int product_weight;
+	private String product_process;
+	private int product_quantity;
+	private String id;
+	
+	public int getProduct_seq() {
+		return product_seq;
+	}
+	public void setProduct_seq(int product_seq) {
+		this.product_seq = product_seq;
+	}
+	public String getProduct_code() {
+		return product_code;
+	}
+	public void setProduct_code(String product_code) {
+		this.product_code = product_code;
+	}
+	public String getVendor_name() {
+		return vendor_name;
+	}
+	public void setVendor_name(String vendor_name) {
+		this.vendor_name = vendor_name;
+	}
+	public String getProduct_type() {
+		return product_type;
+	}
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public int getProduct_weight() {
+		return product_weight;
+	}
+	public void setProduct_weight(int product_weight) {
+		this.product_weight = product_weight;
+	}
+	public String getProduct_process() {
+		return product_process;
+	}
+	public void setProduct_process(String product_process) {
+		this.product_process = product_process;
+	}
+	public int getProduct_quantity() {
+		return product_quantity;
+	}
+	public void setProduct_quantity(int product_quantity) {
+		this.product_quantity = product_quantity;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ src/main/java/com/toyproject/www/VO/VendorVO.java
@@ -0,0 +1,35 @@
+package com.toyproject.www.VO;
+
+public class VendorVO {
+	private int vendor_seq;
+	private String vendor_code;
+	private String vendor_name;
+	private String id;
+	
+	public int getVendor_seq() {
+		return vendor_seq;
+	}
+	public void setVendor_seq(int vendor_seq) {
+		this.vendor_seq = vendor_seq;
+	}
+	public String getVendor_code() {
+		return vendor_code;
+	}
+	public void setVendor_code(String vendor_code) {
+		this.vendor_code = vendor_code;
+	}
+	public String getVendor_name() {
+		return vendor_name;
+	}
+	public void setVendor_name(String vendor_name) {
+		this.vendor_name = vendor_name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
