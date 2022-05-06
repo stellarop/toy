5096973c9205f740be0e8ecc9513bb358f64c6e6
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-05-07 01:16:09 +0900

    깃허브 토큰 재생성 후 푸쉬 테스트

--- src/main/java/com/toyproject/www/VO/VendorVO.java
+++ src/main/java/com/toyproject/www/VO/VendorVO.java
@@ -9,36 +9,7 @@ public class VendorVO {
 	private String vendor_type;
 	private Date regData;
 	
-	public String getVendor_code() {
-		return vendor_code;
-	}
-	public void setVendor_code(String vendor_code) {
-		this.vendor_code = vendor_code;
-	}
-	public String getVendor_name() {
-		return vendor_name;
-	}
-	public void setVendor_name(String vendor_name) {
-		this.vendor_name = vendor_name;
-	}
-	public String getVendor_number() {
-		return vendor_number;
-	} 
-	public void setVendor_number(String vendor_number) {
-		this.vendor_number = vendor_number;
-	}
-	public String getVendor_type() {
-		return vendor_type;
-	}
-	public void setVendor_type(String vendor_type) {
-		this.vendor_type = vendor_type;
-	}
-	public Date getRegData() {
-		return regData;
-	}
-	public void setRegData(Date regData) {
-		this.regData = regData;
-	}
+	
 	
 	
 	
