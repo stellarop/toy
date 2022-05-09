1aacb3117b0b16d1a5c997e3c8d968bfe958c5b3
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-05-10 06:08:33 +0900

    판매처 등록xml / jpa 라이브러리 추가

--- src/main/java/com/toyproject/www/Controller/Maincontroller.java
+++ src/main/java/com/toyproject/www/Controller/Maincontroller.java
@@ -1,13 +1,14 @@
 package com.toyproject.www.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.commons.lang3.StringUtils;
+
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.toyproject.www.Service.UserService;
 import com.toyproject.www.VO.UserVO;
 
 import lombok.extern.log4j.Log4j;
@@ -16,17 +17,17 @@ import lombok.extern.log4j.Log4j;
 @Controller
 public class Maincontroller {
 	private static final Logger logger = Logger.getLogger(Maincontroller.class);
-
-	@Autowired
-	UserService userservice;
-
+	
 	@RequestMapping(value = "/main.do")
-	public String main(UserVO uservo, Model model) {
-		UserVO user = userservice.selectUser(uservo);
-		if (user == null || StringUtils.isEmpty(user.getId())) {
+	public String main(UserVO uservo, Model model, HttpSession session) {
+		
+		String userId = (String) session.getAttribute("userId");
+//		||StringUtils.isEmpty(userId)
+		if (userId == null) {
 			logger.info("[비정상적인 접근] : 로그인 후 이용 바랍니다.");
 			return "redirect:login.jsp";
 		}
+		
 		return "main.jsp";
 	}
 }
