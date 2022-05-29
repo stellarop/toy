2e081eb18516e4a4a34933e2293fd9c3dc1a8c5d
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-05-30 06:05:55 +0900

    ProductVO, VendorVO 등록

--- src/main/java/com/toyproject/www/VO/ProductVO.java
+++ src/main/java/com/toyproject/www/VO/ProductVO.java
@@ -3,77 +3,95 @@ package com.toyproject.www.VO;
 import java.sql.Date;
 
 public class ProductVO {
-	private int product_seq;
-	private String vendor_code;
-	private String product_code;
-	private String vendor_name;
-	private String product_type;
-	private int product_price;
-	private int product_weight;
-	private String product_process;
-	private int product_quantity;
-	private Date regData;
-	public int getProduct_seq() {
-		return product_seq;
-	}
-	public void setProduct_seq(int product_seq) {
-		this.product_seq = product_seq;
-	}
-	public String getVendor_code() {
-		return vendor_code;
-	}
-	public void setVendor_code(String vendor_code) {
-		this.vendor_code = vendor_code;
-	}
-	public String getProduct_code() {
-		return product_code;
-	}
-	public void setProduct_code(String product_code) {
-		this.product_code = product_code;
-	}
-	public String getVendor_name() {
-		return vendor_name;
-	}
-	public void setVendor_name(String vendor_name) {
-		this.vendor_name = vendor_name;
-	}
-	public String getProduct_type() {
-		return product_type;
-	}
-	public void setProduct_type(String product_type) {
-		this.product_type = product_type;
-	}
-	public int getProduct_price() {
-		return product_price;
-	}
-	public void setProduct_price(int product_price) {
-		this.product_price = product_price;
-	}
-	public int getProduct_weight() {
-		return product_weight;
-	}
-	public void setProduct_weight(int product_weight) {
-		this.product_weight = product_weight;
-	}
-	public String getProduct_process() {
-		return product_process;
-	}
-	public void setProduct_process(String product_process) {
-		this.product_process = product_process;
-	}
-	public int getProduct_quantity() {
-		return product_quantity;
-	}
-	public void setProduct_quantity(int product_quantity) {
-		this.product_quantity = product_quantity;
-	}
-	public Date getRegData() {
-		return regData;
-	}
-	public void setRegData(Date regData) {
-		this.regData = regData;
-	}
-	
+	// 상품 번호
+		private int product_seq;
+		// 판매처 고유 번호
+		private int vendor_seq;
+		// 판매처 고유 코드
+		private String vendor_code;
+		// 상품 고유 코드 
+		private String product_code;
+		// 판매처 상호
+		private String vendor_name;
+		// 상품 타입
+		private String product_type;
+		// 상품 가격
+		private int product_price;
+		// 상품 무게
+		private int product_weight;
+		// 상품 가공 여부
+		private String product_process;
+		// 상품 수량
+		private int product_quantity;
+		// 상품 등록 날짜
+		private Date regData;
+		public int getProduct_seq() {
+			return product_seq;
+		}
+		public void setProduct_seq(int product_seq) {
+			this.product_seq = product_seq;
+		}
+		public int getVendor_seq() {
+			return vendor_seq;
+		}
+		public void setVendor_seq(int vendor_seq) {
+			this.vendor_seq = vendor_seq;
+		}
+		public String getVendor_code() {
+			return vendor_code;
+		}
+		public void setVendor_code(String vendor_code) {
+			this.vendor_code = vendor_code;
+		}
+		public String getProduct_code() {
+			return product_code;
+		}
+		public void setProduct_code(String product_code) {
+			this.product_code = product_code;
+		}
+		public String getVendor_name() {
+			return vendor_name;
+		}
+		public void setVendor_name(String vendor_name) {
+			this.vendor_name = vendor_name;
+		}
+		public String getProduct_type() {
+			return product_type;
+		}
+		public void setProduct_type(String product_type) {
+			this.product_type = product_type;
+		}
+		public int getProduct_price() {
+			return product_price;
+		}
+		public void setProduct_price(int product_price) {
+			this.product_price = product_price;
+		}
+		public int getProduct_weight() {
+			return product_weight;
+		}
+		public void setProduct_weight(int product_weight) {
+			this.product_weight = product_weight;
+		}
+		public String getProduct_process() {
+			return product_process;
+		}
+		public void setProduct_process(String product_process) {
+			this.product_process = product_process;
+		}
+		public int getProduct_quantity() {
+			return product_quantity;
+		}
+		public void setProduct_quantity(int product_quantity) {
+			this.product_quantity = product_quantity;
+		}
+		public Date getRegData() {
+			return regData;
+		}
+		public void setRegData(Date regData) {
+			this.regData = regData;
+		}
+		
 	
 	
 	
--- src/main/java/com/toyproject/www/VO/VendorVO.java
+++ src/main/java/com/toyproject/www/VO/VendorVO.java
@@ -3,41 +3,55 @@ package com.toyproject.www.VO;
 import java.sql.Date;
 
 public class VendorVO {
-	private String vendor_code;
-	private String vendor_name;
-	private String vendor_number;
-	private String vendor_type;
-	private Date regData;
-	public String getVendor_code() {
-		return vendor_code;
-	}
-	public void setVendor_code(String vendor_code) {
-		this.vendor_code = vendor_code;
-	}
-	public String getVendor_name() {
-		return vendor_name;
-	}
-	public void setVendor_name(String vendor_name) {
-		this.vendor_name = vendor_name;
-	}
-	public String getVendor_number() {
-		return vendor_number;
-	}
-	public void setVendor_number(String vendor_number) {
-		this.vendor_number = vendor_number;
-	}
-	public String getVendor_type() {
-		return vendor_type;
-	}
-	public void setVendor_type(String vendor_type) {
-		this.vendor_type = vendor_type;
-	}
-	public Date getRegData() {
-		return regData;
-	}
-	public void setRegData(Date regData) {
-		this.regData = regData;
-	}
+	// 판매처 고유 번호
+		private int vendor_seq;
+		// 판매처 고유 코드
+		private String vendor_code;
+		// 판매처 상호
+		private String vendor_name;
+		// 판매 담당자 번호
+		private String vendor_number;
+		// 판매처 상품 타입
+		private String vendor_type;
+		// 등록날짜
+		private Date regData;
+		public int getVendor_seq() {
+			return vendor_seq;
+		}
+		public void setVendor_seq(int vendor_seq) {
+			this.vendor_seq = vendor_seq;
+		}
+		public String getVendor_code() {
+			return vendor_code;
+		}
+		public void setVendor_code(String vendor_code) {
+			this.vendor_code = vendor_code;
+		}
+		public String getVendor_name() {
+			return vendor_name;
+		}
+		public void setVendor_name(String vendor_name) {
+			this.vendor_name = vendor_name;
+		}
+		public String getVendor_number() {
+			return vendor_number;
+		}
+		public void setVendor_number(String vendor_number) {
+			this.vendor_number = vendor_number;
+		}
+		public String getVendor_type() {
+			return vendor_type;
+		}
+		public void setVendor_type(String vendor_type) {
+			this.vendor_type = vendor_type;
+		}
+		public Date getRegData() {
+			return regData;
+		}
+		public void setRegData(Date regData) {
+			this.regData = regData;
+		}
+	
 	
 	
 }
