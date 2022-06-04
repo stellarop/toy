0611be2727d5a7e798a15b7fd078cee7d025b4cc
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-06-05 06:24:06 +0900

    게시글 삭제 메서드 / logger

--- src/main/java/com/toyproject/www/Controller/SimpleBoardController.java
+++ src/main/java/com/toyproject/www/Controller/SimpleBoardController.java
@@ -3,6 +3,7 @@ package com.toyproject.www.Controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,7 @@ import com.toyproject.www.VO.SimpleBoardVO;
 
 @Controller
 public class SimpleBoardController {
+	private static final Logger logger = Logger.getLogger(Maincontroller.class);
 	@Autowired
 	private SimpleBoardService simpleboardservice;
 	
@@ -23,6 +25,7 @@ public class SimpleBoardController {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		//dataTable로 넘겨주는 데이터 이름은 무조건 data로 넘겨주어야 한다.
 		map.put("data", simpleboardservice.boardList(vo));
+		logger.info("boardList 실행");
 		return map;
 	}
 	
@@ -30,7 +33,20 @@ public class SimpleBoardController {
 	@RequestMapping(value = "/getBoard.do")
 	public String getBoard(SimpleBoardVO vo, Model model) {
 		model.addAttribute("getBoard", simpleboardservice.getBoard(vo));
+		logger.info("getBoard 실행");
 		return "getBoard.jsp";
+	}
+	
+	public boolean deleteBoard(SimpleBoardVO vo) {
+		
+		try {
+			simpleboardservice.deleteBoard(vo);
+			logger.info("게시글 삭제 완료");
+			return true;
+		} catch (Exception e) {
+			logger.error("게시글 삭제 실패 [" + e.getMessage() + "]");
+			return false;
+		}
 	}
 	
 	
