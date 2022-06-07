e246f65065ac48dfd8c11e5946dca89cdbac282d
leeyeonjae <leeyeonjae@DESKTOP-FN07PJ9>
2022-06-07 16:56:01 +0900

    예외처리

--- src/main/java/com/toyproject/www/Controller/SimpleBoardController.java
+++ src/main/java/com/toyproject/www/Controller/SimpleBoardController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.toyproject.www.Service.SimpleBoardService;
 import com.toyproject.www.VO.SimpleBoardVO;
@@ -24,25 +25,36 @@ public class SimpleBoardController {
 	@RequestMapping(value = "/boardList.do")
 	public Map<String, Object> boardList(SimpleBoardVO vo) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		//dataTable로 넘겨주는 데이터 이름은 무조건 data로 넘겨주어야 한다.
-		map.put("data", simpleboardservice.boardList(vo));
-		logger.info("boardList 실행");
+		try {
+			//dataTable로 넘겨주는 데이터 이름은 무조건 data로 넘겨주어야 한다.
+			map.put("data", simpleboardservice.boardList(vo));
+			logger.info("[boardList 실행]");
+		} catch (Exception e) {
+			logger.error("boardList 오류[" + e.getMessage() + "]");
+		}
+		
+		
 		return map;
 	}
 	
 	
 	@RequestMapping(value = "/getBoard.do")
 	public String getBoard(SimpleBoardVO vo, Model model) {
-		model.addAttribute("getBoard", simpleboardservice.getBoard(vo));
-		logger.info("getBoard 실행");
+		try {
+			model.addAttribute("getBoard", simpleboardservice.getBoard(vo));
+			logger.info("[getBoard 실행]");
+		} catch (Exception e) {
+			logger.error("getBoard 오류[" + e.getMessage() + "]");
+		}
+		
 		return "getBoard.jsp";
 	}
 	
-	@RequestMapping(value = "/deleteBoard.do", method = RequestMethod.DELETE)
-	public boolean deleteBoard(SimpleBoardVO vo) {
-		
+	@RequestMapping(value = "/deleteBoard.do")
+	public boolean deleteBoard(int board_seq) {
+		logger.info("board_seq : " + board_seq);
 		try {
-			simpleboardservice.deleteBoard(vo);
+			simpleboardservice.deleteBoard(board_seq);
 			logger.info("게시글 삭제 완료");
 			return true;
 		} catch (Exception e) {
